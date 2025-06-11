45a03134e19164da9d2f0b76b853d360b1e5098c
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2025-06-11 21:47:23 +0300

    Step #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,63 @@
 #include <Arduino.h>
+
+#define RELAY_PIN A0
+
+#define FLOW_SENSOR_PIN A6
+
+#define COVER_SW_PIN    13
+
+#define SPK_PIN_A       4
+#define SPK_PIN_B       5
+
+#define FLOW_THRESHOLD_MS 100
+
+uint8_t flowPhase;
+int flowPhaseCounterMs = 0;
+uint8_t spkPhase = 0;
+
+
+void init_relay()
+{
+    digitalWrite(RELAY_PIN, LOW);
+    pinMode(RELAY_PIN, OUTPUT);
+}
+void init_sound()
+{
+    digitalWrite(SPK_PIN_A, HIGH);
+    digitalWrite(SPK_PIN_B, HIGH);
+    pinMode(SPK_PIN_A, OUTPUT);
+    pinMode(SPK_PIN_B, OUTPUT);
+}
+
+void init_sensors()
+{
+    pinMode(FLOW_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(COVER_SW_PIN, INPUT_PULLUP);
+}
+
 void setup() {
-// write your initialization code here
+    init_relay();
+    init_sensors();
+    init_sound();
+}
+
+void sound_loop(bool alarm) {
+    if (alarm)
+    {
+        digitalWrite(SPK_PIN_A, spkPhase);
+        spkPhase = !spkPhase;
+        digitalWrite(SPK_PIN_B, spkPhase);
+    } else
+    {
+        digitalWrite(SPK_PIN_A, HIGH);
+        digitalWrite(SPK_PIN_B, HIGH);
+    }
 }
 
 void loop() {
-// write your code here
+    bool alarm = true;
+    alarm = (digitalRead(COVER_SW_PIN) == LOW);
+    digitalWrite(RELAY_PIN, alarm ? LOW : HIGH);
+    // sound_loop(alarm);
+    delay(1);
 }
